0322e43ca0a5361e77e4dba0d4341f626129c310
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-21 11:07:12 -0400

    Gerard Square 1.0  /main.js

--- main.js
+++ main.js
@@ -75,7 +75,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
                 social_media: [
                     {
                         name: "Facebook",
-                        url: "https://www.facebook.com",
+                        url: "https://www.facebook.com/GerrardSquare",
                         iconClass: "fab fa-facebook-f"
                     }
                 ],
