c1c2c280b389309c23f87ff5299d87f4071a5bc6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-25 12:02:41 -0400

    Gerard Square 1.0  /main.js

--- main.js
+++ main.js
@@ -111,7 +111,8 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
             },
             ...Vuex.mapGetters([
                 'dataLoaded',
-                'property'
+                'property',
+                'findRepoByName'
             ])
         },
         methods: {
