b931b93dedc31caa3a42273ee7263f3004290d99
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-25 12:18:06 -0400

    Gerard Square 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -74,7 +74,7 @@
         Vue.use(VueLazyload);
         return Vue.component("promotions-component", {
             template: template, // the variable template will be injected,
-            props:['menu_items', 'social_media'],
+            props:['menu_items', 'inside_banner'],
             data: function () {
                 return {
                     dataLoaded: false,
