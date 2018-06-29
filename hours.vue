c1bb06cb1d379edb24da735ae4280af4a009bd7d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-29 16:43:13 -0400

    Gerard Square 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -3,7 +3,7 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" class="main_container margin_30" v-cloak> 
-            <div class="inside_header_background" :style="{ backgroundImage: 'url(' + inside_banner.image_url + ')' }">
+                <div class="inside_header_background" :style="{ backgroundImage: 'url(' + inside_banner.image_url + ')' }">
                     <div class="main_container">
                         <h2>Jobs</h2>
                     </div>
