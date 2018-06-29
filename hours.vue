1b9b7a1b29500b10dce2e4d33be82a53164640a8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-29 16:44:16 -0400

    Gerard Square 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -5,7 +5,7 @@
             <div v-if="dataLoaded" v-cloak>
                 <div class="inside_header_background" :style="{ backgroundImage: 'url(' + inside_banner.image_url + ')' }">
                     <div class="main_container">
-                        <h2>Jobs</h2>
+                        <h2>Hours</h2>
                     </div>
                 </div>
                 <div class="main_container mobile_padding margin_30"><div v-if="dataLoaded" v-cloak>
