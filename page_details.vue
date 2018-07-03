cc582d9303266b1cacac1afeca81db2be98c3fe8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-07-03 15:56:45 -0400

    Gerard Square 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -16,7 +16,12 @@
                         <div class="details_col_9">
                             <div class="page_body" v-html="currentPage.body"></div>
                             <div v-if="leasingImages">
-                                
+                                <div v-for="image in images" class="col-md-6 leasing_photo_container">
+                                    <a :href="image.image_url" :data-lightbox="image.image_url">
+                                        <div class="leasing_photo" v-lazy:background-image="image.image_url"></div>
+                                        <p style="display:none;">{{image.id}}</p>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
