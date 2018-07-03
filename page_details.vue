3cea8fbc69e2e5f2668adc5e03d0ef97aa22133a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-07-03 15:59:47 -0400

    Gerard Square 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -64,7 +64,7 @@
                     this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/" + _this.id + ".json" }).then(function (response) {
                         _this.currentPage = response.data;
                         if(_.includes(_this.id,'leasing')) {
-                            var repo_data = this.findRepoByName("Leasing Images");
+                            var repo_data = _this.findRepoByName("Leasing Images");
                             if(repo_data != null && repo_data !== undefined && repo_data.images.length > 0){
                                 this.leasingImages = repo_data.images;
                                 console.log("this.leasingImages", this.leasingImages)
