bd3e108bba16507feae3b4a00d197ec7db88803a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-13 12:06:49 -0400

    Gerard Square 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -19,22 +19,22 @@
                                     <p>We value your feedback. Send us your comments or questions by completing the contact form below.</p>    
                                 </div>
                                 <form class="form-horizontal clearfix" action="form-submit" v-on:submit.prevent="validateBeforeSubmit">
-                                    <div class="col-xs-12 col-md-6" :class="{'has-error': errors.has('name')}">
+                                    <div class="col-xs-12 col-md-6 margin_20" :class="{'has-error': errors.has('name')}">
                                         <label for="name">Name</label>
                                         <input id="name" v-model="form_data.name" v-validate="'required|true'" class="form-control" :class="{'input': true}" name="name" type="text" data-vv-delay="1000">
                                         <span v-show="errors.has('name')" class="form-control-feedback">{{ errors.first('name') }}</span>
                                     </div>
-                                    <div class="col-xs-12 col-md-6" :class="{'has-error': errors.has('email')}">
+                                    <div class="col-xs-12 col-md-6 margin_20" :class="{'has-error': errors.has('email')}">
                                         <label for="email">Email</label>
                                         <input id="email" v-model="form_data.email" v-validate="'required|email'" class="form-control" :class="{'input': true}" name="email" type="email" data-vv-delay="1000">
                                         <span v-show="errors.has('email')" class="form-control-feedback">{{ errors.first('email') }}</span>
                                     </div>
-                                    <div class="col-xs-12" :class="{'has-error': errors.has('subject')}">
+                                    <div class="col-xs-12 margin_20" :class="{'has-error': errors.has('subject')}">
                                         <label for="subject">Subject</label>
                                         <input id="subject" v-model="form_data.subject" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="subject" type="text" data-vv-delay="1000">
                                         <span v-show="errors.has('subject')" class="form-control-feedback">{{ errors.first('subject') }}</span>
                                     </div>
-                                    <div class="col-xs-12" :class="{'has-error': errors.has('message')}">
+                                    <div class="col-xs-12 margin_20" :class="{'has-error': errors.has('message')}">
                                         <label for="message">Message</label>
                                         <textarea id="message" v-model="form_data.message" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="message" type="text" data-vv-delay="1000"></textarea>
                                         <span v-show="errors.has('message')" class="form-control-feedback">{{ errors.first('message') }}</span>
