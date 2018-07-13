0790e64c01ce1438fef029511452abc292723632
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-13 11:49:18 -0400

    Gerard Square 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -3,7 +3,7 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-                <div class="inside_header_background" :style="{ backgroundImage: 'url(' + inside_banner.image_url + ')' }">
+                <div class="inside_header_background" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
                     <div class="main_container">
                         <h2>Hours</h2>
                     </div>
@@ -68,11 +68,21 @@
             props:['inside_banner'],
             data: function data() {
                 return {
-                    dataLoaded: false
+                    dataLoaded: false,
+                    pageBanner: null
                 }
             },
             created() {
                 this.loadData().then(response => {
+                    var temp_repo = this.findRepoByName('Hours Banner').images;
+                    if(temp_repo != null) {
+                        this.pageBanner = temp_repo[0];
+                    } else {
+                        this.pageBanner = {
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b2925776e6f6432b6110000/image/png/1531495616000/inside_banner.png"
+                        }
+                    }
+                    
                     this.dataLoaded = true;
                 });
             },
@@ -80,6 +90,7 @@
                 ...Vuex.mapGetters([
                     'property',
                     'timezone',
+                    'findRepoByName',
                     'getPropertyHours',
                     'getPropertyHolidayHours'
                 ]),
