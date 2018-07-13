06a3e1469e1332eaf1c9545ab198f2807ea26ab7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-13 11:45:48 -0400

    Gerard Square 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -83,6 +83,21 @@
 					console.error("Could not retrieve data from server. Please check internet connection and try again.");
 				});
 			},
+			watch: {
+                currentEvent : function (){
+                    if(this.currentEvent != null) {
+                        if (this.currentEvent.eventable_type === "Store"){
+                            if (_.includes(this.currentEvent.event_image_url_abs, 'missing')) {
+                                this.currentEvent.image_url = this.currentEvent.store.store_front_url_abs; 
+                            }
+                        } else {
+                            if (_.includes(this.currentEvent.event_image_url_abs, 'missing')) {
+                                this.currentEvent.image_url = "//codecloud.cdn.speedyrails.net/sites/5b2925776e6f6432b6110000/image/png/1531496511000/event placeholder.png";    
+                            }
+                        }
+                    }
+                }
+            },
 			computed: {
 				...Vuex.mapGetters([
 					'property',
