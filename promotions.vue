7a4bbb4a639a486e53a9cf3bbe671dc91ba22fcb
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-13 11:31:21 -0400

    Gerard Square 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -3,7 +3,7 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-                <div class="inside_header_background" :style="{ backgroundImage: 'url(' + inside_banner.image_url + ')' }">
+                <div class="inside_header_background" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
                     <div class="main_container">
                         <h2>Promotions</h2>
                     </div>
@@ -78,12 +78,22 @@
             data: function () {
                 return {
                     dataLoaded: false,
+                    pageBanner: null,
                     toggleEvents: false,
                     togglePromos: false
                 }
             },
             created (){
                 this.loadData().then(response => {
+                    var temp_repo = this.findRepoByName('Promotions Banner').images;
+                    if(temp_repo != null) {
+                        this.pageBanner = temp_repo[0];
+                    } else {
+                        this.pageBanner = {
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b2925776e6f6432b6110000/image/png/1531495616000/inside_banner.png"
+                        }
+                    }
+                    
                     this.dataLoaded = true;
                     console.log("inside_banner", this.inside_banner)
                 });
