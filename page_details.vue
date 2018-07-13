dd8372f6dc11503d9c9d51af8faca53d1f76fdb2
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-13 12:31:16 -0400

    Gerard Square 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -3,7 +3,7 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-                <div class="inside_header_background" :style="{ backgroundImage: 'url(' + inside_banner.image_url + ')' }">
+                <div class="inside_header_background" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
                     <div class="main_container">
                         <h2 v-html="currentPage.title"></h2>
                     </div>
@@ -36,10 +36,11 @@
         Vue.use(Lightbox);
         return Vue.component("page-details-component", {
             template: template, // the variable template will be injected,
-            props: ['id','inside_banner'],
+            props: ['id'],
             data: function data() {
                 return {
                     dataLoaded: false,
+                    pageBanner: null,
                     currentPage: null,
                     leasingImages : null
                 }
