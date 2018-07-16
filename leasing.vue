8ae1a6cf9112cb792cc9bcb5d43441dedd8ce077
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-16 11:51:08 -0400

    Gerard Square 1.0  /leasing.vue

--- leasing.vue
+++ leasing.vue
@@ -199,6 +199,17 @@
                             // });
                         }
                     })
+                },
+                serializeObject(obj) {
+                    console.log(obj);
+                    var newObj = [];
+                    _.forEach(obj, function(value, key) {
+                        var tempVal = {};
+                        tempVal.name = key;
+                        tempVal.value = value;
+                        newObj.push(tempVal);
+                    });
+                    return newObj;
                 }
             }
         });
