6b47d500b16989ddfaf1e46eebe9c7c2ec354e2e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-16 11:40:41 -0400

    Gerard Square 1.0  /leasing.vue

--- leasing.vue
+++ leasing.vue
@@ -88,12 +88,12 @@
         				</div>
     				</div>
     			</div>
-    			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2886.0099599626933!2d-79.3415429844806!3d43.668762679120704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb7ece9e16db%3A0x7e63831ebf683b46!2sGerrard+Square+Shopping+Centre!5e0!3m2!1sen!2sca!4v1531755543275" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2886.0099599626933!2d-79.3415429844806!3d43.668762679120704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb7ece9e16db%3A0x7e63831ebf683b46!2sGerrard+Square+Shopping+Centre!5e0!3m2!1sen!2sca!4v1531755543275" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
 				<div class="main_container mobile_padding margin_30">
                     <div class="row">
                         <div class="col-md-12">
             				<div>
-            				    <h3 class="inside_page_header">Specialty Leasing</h3>
+            				    <h3 class="inside_page_header">Specialty Short Term Leasing</h3>
                                 <div class="margin_40 page_body" v-if="tempLeasing" v-html="tempLeasing.body"></div>
             				</div>
         				</div>
