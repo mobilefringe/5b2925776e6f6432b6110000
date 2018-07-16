5bda3f64d852e7827c0031e8e9d55906f879dad0
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-16 15:56:42 -0400

    Gerard Square 1.0  /leasing.vue

--- leasing.vue
+++ leasing.vue
@@ -12,7 +12,16 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="page_body" v-html="currentPage.body"></div>
-                            <div v-if="leasingImages" class="leasing_images">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="page_body" v-html="currentPage.body"></div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div v-if="leasingImages" class="row leasing_images">
                                 <div v-for="image in leasingImages" class="col-md-4 leasing_photo_container">
                                     <a :href="image.image_url" data-lightbox="leasing images">
                                         <div class="leasing_photo" :style="{ backgroundImage: 'url(' +image.image_url+ ')' }"></div>
