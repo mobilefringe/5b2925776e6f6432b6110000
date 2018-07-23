4f035b3054a5b8de953eb3b11bc4ae43b0beb418
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-07-23 17:25:29 -0400

    Gerrard Square 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -162,9 +162,6 @@
                     if (this.currentStore.store_hours) {
                         var storeHours = [];
                         _.forEach(this.currentStore.store_hours, function (value, key) {
-                            if(value.open_full_day) {
-                                
-                            }
                             storeHours.push(vm.findHourById(value));
                         });
                         this.storeHours = storeHours;
