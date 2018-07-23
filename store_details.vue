672af9e2bc7e7babdec8ee5d47070dfd296d7444
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-07-23 17:27:50 -0400

    Gerrard Square 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -28,15 +28,15 @@
                             </div>
                             <div class="inside_page_header">Store Hours & Information</div>
                             <ul v-if="storeHours" class="store_details_hours_list">
-                                <li v-for="hour in storeHours" :class="{ today: hour.todays_hours }">
+                                <li v-for="hour in storeHours" :class="{ today: hour.todays_hours }">{{hour.open_full_day}}
                                     <div v-if="!hour.is_closed">
-                                        <span class="hours_list_day">{{hour.day_of_week | moment("dddd", timezone)}} </span>{{hour.open_time | moment("h:mma", timezone)}} - {{hour.close_time | moment("h:mma", timezone)}}
+                                        <span class="hours_list_day">{{hour.day_of_week | moment("dddd", timezone)}} </span>CLOSED
                                     </div>
                                     <div v-else-if="hour.open_full_day">
                                         <span class="hours_list_day">{{hour.day_of_week | moment("dddd", timezone)}} </span>OPEN 24 HOURS
                                     </div>
                                     <div v-else>
-                                        <span class="hours_list_day">{{hour.day_of_week | moment("dddd", timezone)}} </span>CLOSED
+                                        <span class="hours_list_day">{{hour.day_of_week | moment("dddd", timezone)}} </span>{{hour.open_time | moment("h:mma", timezone)}} - {{hour.close_time | moment("h:mma", timezone)}}
                                     </div>
                                 </li>
                             </ul>
