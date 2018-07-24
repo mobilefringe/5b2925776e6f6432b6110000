3484b06f27abc5f3730eb1469a44932d70e1dbc4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-07-24 12:07:15 -0400

    Gerrard Square 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -6,7 +6,7 @@
                 <div class="inside_header_background" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
                     <div class="main_container">
                         <div class="page_container">
-                            <h2>Center Map</h2>
+                            <h2>Centre Map</h2>
                         </div>
                     </div>
                 </div>
